f8c5972d9ba42056dbea98efffe3ae24b3627538
Sergey Nelepa <nelepa1995@mail.ru>
2016-06-18 17:07:34 +0300

    add test for shorter and expand links

--- shorter/tests.py
+++ shorter/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from shorter.models import Link
 # Create your tests here.
+
+
+class ShorterTest(TestCase):
+
+    def test_shorter(self):
+        url = 'http://testing_long.url/test'
+        link = Link(url=url)
+        short_link = Link.shorter(link)
+        self.assertLess(len(short_link), len(short_link))
+
+    def test_expand(self):
+        url = 'http://testing_long.url/test'
+        link = Link(url=url)
+        short_link = Link.shorter(link)
+        link.save()
+        expand_url = Link.expand(short_link)
+        self.assertEqual(expand_url, url)
+
+
+
+
